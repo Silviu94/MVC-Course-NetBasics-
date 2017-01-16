ad1eed671379c7691d4ad5117e6031cafa81e7e4
Silviu <Silviu>
2017-01-16 16:46:31 +0200

    course 6  exercise 8

---
+++ ProjectManagement/Controllers/ActionResultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class ActionResultController : Controller
+    {
+        // GET: ActionResult
+        public ActionResult Index()
+        {
+            return Content("This message is sent from the index action");
+        }
+        public ActionResult RedirectPermanent()
+        {
+            return RedirectPermanent("http://www.google.ro");
+        }
+        public ActionResult RedirectToActionInTheSameController()
+        {
+            return RedirectToAction("Index");
+        }
+
+    }
+}
